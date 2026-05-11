318244c5bc7338f7070003b56bed061804a274c3
Facundo-caceres-t <facucaceres180@gmail.com>
2026-05-10 23:41:11 -0300

    creacion de estructuras para los casilleros
    A la par se crean los .c y .h para el manejo del tablero. A donde apunta la información de cada nodo de la lista es a una estructura de casillero

---
+++ Tablero.h
@@ -0,0 +1,25 @@
+#ifndef TABLERO_H_INCLUDED
+#define TABLERO_H_INCLUDED
+
+typedef struct tBandido tBandido; //Solo informa que existe el tipo de datos para evitar dependencias circulares entre headers
+
+typedef enum
+{
+    DESPEJADO,
+    INICIO,
+    SALIDA,
+    PREMIO,
+    VIDA_EXTRA,
+    OASIS,
+    TORMENTA
+} tTipoEvento;  //Asigna valores distintos a cada evento para no hacer muchos defines
+
+typedef struct
+{
+    tTipoEvento tipoEvento;
+    int tieneJugador;
+    int numeroCasillero;
+    tBandido *bandido;  //Si hay un bandido en el casillero, permite pasarlo a inactivo directamente
+} tCasillero;
+
+#endif // TABLERO_H_INCLUDED
